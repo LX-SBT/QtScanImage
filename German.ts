d57de778823a97a4ca47a745a90ba2da3352e9e9
LX-SBT <72764632+LX-SBT@users.noreply.github.com>
2022-05-31 20:10:37 +0200

    Add files via upload

--- German.ts
+++ German.ts
@@ -5,7 +5,7 @@
     <name>QtScanImage</name>
     <message>
         <location filename="qtscanimage.ui" line="60"/>
-        <location filename="qtscanimage.ui" line="814"/>
+        <location filename="qtscanimage.ui" line="840"/>
         <source>Scan</source>
         <translation>Scannen</translation>
     </message>
@@ -25,17 +25,17 @@
         <translation>speichern</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="130"/>
+        <location filename="qtscanimage.ui" line="156"/>
         <source>Target &amp;Path:</source>
         <translation>Ziel&amp;pfad:</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="175"/>
+        <location filename="qtscanimage.ui" line="201"/>
         <source>Paper&amp;size</source>
         <translation>Papier&amp;größe</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="243"/>
+        <location filename="qtscanimage.ui" line="295"/>
         <source>&amp;Height</source>
         <translation>&amp;Höhe</translation>
     </message>
@@ -45,132 +45,171 @@
         <translation>&amp;Breite</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="298"/>
+        <location filename="qtscanimage.ui" line="130"/>
+        <source>Device:</source>
+        <translation>Gerät:</translation>
+    </message>
+    <message>
+        <location filename="qtscanimage.ui" line="143"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;1. connect the scanner to the computer&lt;/p&gt;&lt;p&gt;2. determine device with &amp;quot;&lt;span style=&quot; font-weight:600;&quot;&gt;scanimage -L&lt;/span&gt;&amp;quot; in the terminal&lt;/p&gt;&lt;p&gt;for example: &lt;span style=&quot; font-weight:600;&quot;&gt;dsseries:usb:0x04F9:0x60E0&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;1. Verbinde den Scanner mit dem Computer&lt;/p&gt;&lt;p&gt;2. Bestimme das Gerät mit &amp;quot;&lt;span style=&quot; font-weight:600;&quot;&gt;scanimage -L&lt;/span&gt;&amp;quot; im Terminal&lt;/p&gt;&lt;p&gt;Zum Beispiel: &lt;span style=&quot; font-weight:600;&quot;&gt;dsseries:usb:0x04F9:0x60E0&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtscanimage.ui" line="149"/>
+        <source>dsseries:usb:0x04F9:0x60E0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtscanimage.ui" line="324"/>
         <source>Offset</source>
         <translation>Abstand</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="304"/>
+        <location filename="qtscanimage.ui" line="330"/>
         <source>&amp;Top</source>
         <translation>&amp;Oben</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="330"/>
+        <location filename="qtscanimage.ui" line="356"/>
         <source>&amp;Left</source>
         <translation>&amp;Links</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="385"/>
+        <location filename="qtscanimage.ui" line="411"/>
         <source>&amp;Mode</source>
         <translation>&amp;Modus</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="395"/>
+        <location filename="qtscanimage.ui" line="421"/>
         <source>LineArt</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="435"/>
+        <location filename="qtscanimage.ui" line="461"/>
         <source>&amp;Duplex</source>
         <translation>&amp;Duplex</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="446"/>
+        <location filename="qtscanimage.ui" line="472"/>
         <source>File&amp;name:</source>
         <translation>Datei&amp;name:</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="456"/>
+        <location filename="qtscanimage.ui" line="482"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;%d page counter&lt;/p&gt;&lt;p&gt;%p hostfolder name&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;%d Seitenzähler&lt;/p&gt;&lt;p&gt;%p Hostordnername&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="494"/>
+        <location filename="qtscanimage.ui" line="520"/>
         <source>Start &amp;counting from</source>
         <translation>&amp;Zähle ab</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="533"/>
+        <location filename="qtscanimage.ui" line="559"/>
         <source>Enhancement:</source>
         <translation>Korrekturen:</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="584"/>
+        <location filename="qtscanimage.ui" line="610"/>
         <source>&amp;Contrast</source>
         <translation>&amp;Kontrast</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="623"/>
+        <location filename="qtscanimage.ui" line="649"/>
         <source>&amp;Green-Gamma</source>
         <translation>&amp;Grün-Gamma</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="646"/>
+        <location filename="qtscanimage.ui" line="672"/>
         <source>&amp;Blue-Gamma</source>
         <translation>&amp;Blau-Gamma</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="701"/>
+        <location filename="qtscanimage.ui" line="727"/>
         <source>Br&amp;ightness</source>
         <translation>H&amp;elligkeit</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="711"/>
+        <location filename="qtscanimage.ui" line="737"/>
         <source>&amp;Resolution (dpi)</source>
         <translation>&amp;Auflösung</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="737"/>
+        <location filename="qtscanimage.ui" line="763"/>
         <source>Gamma-Correction</source>
         <translation>Gammakorrektur</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="757"/>
+        <location filename="qtscanimage.ui" line="783"/>
         <source>Re&amp;d-Gamma</source>
         <translation>&amp;Rot-Gamma</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="824"/>
-        <location filename="qtscanimage.ui" line="976"/>
+        <location filename="qtscanimage.ui" line="850"/>
+        <location filename="qtscanimage.ui" line="929"/>
         <source>Convert</source>
         <translation>Konvertieren</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="836"/>
+        <location filename="qtscanimage.ui" line="862"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;delete source files after convertion&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <oldsource>delete source files after conertion</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Quelldateien nach der Konvertierung löschen&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="839"/>
+        <location filename="qtscanimage.ui" line="865"/>
         <source>delete source files</source>
         <translation>Quelldatei löschen</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="852"/>
+        <location filename="qtscanimage.ui" line="945"/>
         <source>Compress</source>
         <translation>Kompremierung</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="928"/>
+        <location filename="qtscanimage.ui" line="1028"/>
+        <source>Preferences</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="qtscanimage.ui" line="1038"/>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="qtscanimage.ui" line="881"/>
         <source>&amp;Quality</source>
         <translation>&amp;Qualität</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="982"/>
-        <location filename="qtscanimage.ui" line="994"/>
         <source>OCR</source>
-        <translation>OCR</translation>
+        <translation type="vanished">OCR</translation>
     </message>
     <message>
-        <location filename="qtscanimage.ui" line="1007"/>
-        <source>optical character recognition</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qtscanimage.ui" line="1028"/>
+        <location filename="qtscanimage.ui" line="1023"/>
         <source>&amp;About</source>
         <translation>&amp;Über</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="about.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>language_dialog</name>
+    <message>
+        <location filename="language_dialog.ui" line="14"/>
+        <source>Language</source>
+        <translation type="unfinished">Sprache</translation>
+    </message>
+    <message>
+        <location filename="language_dialog.ui" line="25"/>
+        <source>Language:</source>
+        <translation type="unfinished">Sprache:</translation>
     </message>
 </context>
 </TS>
